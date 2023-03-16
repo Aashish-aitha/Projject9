f7b5daf6d82cd9ce58d2121676d629998e271310
root <root@ip-172-31-7-59.us-east-2.compute.internal>
2023-03-16 00:07:39 +0000

    read all files

---
+++ feature/feature/src/main/resources/dbscripts/oracle.sql
@@ -0,0 +1,9 @@
+
+-- -----------------------------------------------------
+-- Table `Aashish_DEVICE`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS Aashish_DEVICE (
+  Aashish_DEVICE_ID VARCHAR(45) NOT NULL ,
+  DEVICE_NAME VARCHAR(100) NULL DEFAULT NULL,
+  PRIMARY KEY (Aashish_DEVICE_ID) );
+
